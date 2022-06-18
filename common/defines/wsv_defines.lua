44c1c5638f9cd948364f93364bd0c58b8b0a315d
David M. Gaviria <davidmgaviria123@gmail.com>
2022-06-18 00:07:42 -0400

    Batch ww1 upgrades
    Did a ton of ww1 batch upgrades, including
    -Modified tech and tech choosing
    -Modified country oob and templates
    -Modified national focus
    -Added generic character advisors

--- common/defines/wsv_defines.lua
+++ common/defines/wsv_defines.lua
@@ -14,12 +14,12 @@ NDefines.NCountry.BASE_STABILITY_WAR_FACTOR = -0.2
 
 NDefines.NCharacter.DEFAULT_PP_COST_FOR_MILITARY_ADVISOR = 100
 
---NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_LAND_DOCTRINE = 0.75   -- How quickly is desire to unlock land doctrines accumulated?
---NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_NAVAL_DOCTRINE = 0.75   -- How quickly is desire to unlock naval doctrines accumulated?
---NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_AIR_DOCTRINE = 0.6     -- How quickly is desire to unlock air doctrines accumulated?
+NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_LAND_DOCTRINE = 0.65   -- How quickly is desire to unlock land doctrines accumulated?
+NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_NAVAL_DOCTRINE = 0.5   -- How quickly is desire to unlock naval doctrines accumulated?
+NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_AIR_DOCTRINE = 0.5     -- How quickly is desire to unlock air doctrines accumulated?
 
-NDefines.NAI.START_TRAINING_EQUIPMENT_LEVEL = 0.8               -- ai will not start to train if equipment drops below this level
-NDefines.NAI.STOP_TRAINING_EQUIPMENT_LEVEL = 0.75                -- ai will not train if equipment drops below this level
+NDefines.NAI.START_TRAINING_EQUIPMENT_LEVEL = 0.65               -- ai will not start to train if equipment drops below this level
+NDefines.NAI.STOP_TRAINING_EQUIPMENT_LEVEL = 0.5                -- ai will not train if equipment drops below this level
 
 
 --[[
