2be9ae1cce966fc9250df50de73176430cc5bb35
David G. <85266598+davidmgaviria@users.noreply.github.com>
2024-07-21 17:59:09 -0400

    Changes

--- common/defines/wsv_defines.lua
+++ common/defines/wsv_defines.lua
@@ -14,8 +14,12 @@ NDefines.NCountry.WAR_SUPPORT_OFFNSIVE_WAR = -0.2				    -- vanilla is 0.2
 NDefines.NCountry.WAR_SUPPORT_DEFENSIVE_WAR = 0.2				    -- vanilla is 0.2
 NDefines.NCountry.MAX_BOMBING_WAR_SUPPORT_IMPACT = -0.1 					-- Vanilla is -0.3, it is not ww2
 NDefines.NCountry.MAX_CONVOYS_BEING_RAIDED_WAR_SUPPORT_IMPACT = -0.5 		-- Vanilla is -0.3, it is not ww2
-NDefines.NDiplomacy.TENSION_DECAY_DAILY = 0.003											-- vanilla is 0.005
- 
+NDefines.NDiplomacy.TENSION_DECAY_DAILY = 0.003								-- vanilla is 0.005
+
+-- Major
+NDefines.NCountry.ADDITIONAL_MAJOR_COUNTRIES_IC_RATIO = 0.8         -- vanilla is 0.7 (dont want any random idiot being a major)       
+NDefines.NCountry.MAJOR_MIN_FACTORIES = 50						    -- vanilla is 35 (dont want any random idiot being a major)
+
 -- Politics stats
 NDefines.NCharacter.DEFAULT_PP_COST_FOR_MILITARY_ADVISOR = 100  	-- vanilla is 50
 NDefines.NAI.POLITICAL_ADVISOR_SCORE_MULTIPLIER = 2.5           	-- vanilla is 1.25
@@ -27,6 +31,8 @@ NDefines.NAI.NAVY_CHIEF_SCORE_MULTIPLIER = 0.8                  	-- vanilla is 2
 -- Combat stats
 NDefines.NMilitary.DIG_IN_FACTOR = 0.04								-- vanilla is 0.02, entrenchment should be more effective
 NDefines.NMilitary.UNIT_DIGIN_SPEED = 0.5 							-- vanilla is 1, takes longer to dig in
+
+
 
 -- turned off these as we changed casualty loss
 --NDefines.NMilitary.ENTRENCHMENT_WEIGHT = 5.0		 				-- vanilla is 2.0, double it cause we doubled entrenchment
